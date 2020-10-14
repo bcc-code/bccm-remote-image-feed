c0ac04eb9f51467ba70756722ea1940dc13f2324
Andreas Gangsø <andreasgangso@gmail.com>
2020-10-14 12:24:11 +0200

    tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-blue-900 w-screen h-screen">
+    <Feed/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Feed from './components/Feed.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Feed
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'assets/css/tailwind.css';
 </style>
---
+++ frontend/src/components/Feed.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex p-24 text-white">
+        <div class="shadow-lg bg-gray-900 p-6 rounded-xl">
+            a
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
